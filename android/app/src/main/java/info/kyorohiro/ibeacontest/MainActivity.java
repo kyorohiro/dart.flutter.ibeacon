4d09df74bf678d4094c3e56a4f632564f7f0e81e
kyorohiro <kyorohiro@gmail.com>
2017-01-13 13:54:17 +0900

    run dart code on android

--- android/app/src/main/java/info/kyorohiro/ibeacontest/MainActivity.java
+++ android/app/src/main/java/info/kyorohiro/ibeacontest/MainActivity.java
@@ -1,10 +1,27 @@
 package info.kyorohiro.ibeacontest;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import io.flutter.view.FlutterMain;
+import io.flutter.view.FlutterView;
 
 /**
  * Created by kyorohiro on 2017/01/13.
  */
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FlutterMain.ensureInitializationComplete(this,null);
+        LinearLayout rootLayout = new LinearLayout(this);
+        this.setContentView(rootLayout);
+
+        FlutterView flutterView = new FlutterView(this);
+        rootLayout.addView(flutterView);
+
+        flutterView.runFromBundle(FlutterMain.findAppBundlePath(getApplicationContext()), null);
+    }
 }
