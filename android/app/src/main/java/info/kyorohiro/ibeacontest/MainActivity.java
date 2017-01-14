2134d4a2cc8bfbc2c4c715364c52275239bcd41e
kyorohiro <kyorohiro@gmail.com>
2017-01-15 06:41:56 +0900

    temporary save

--- android/app/src/main/java/info/kyorohiro/ibeacontest/MainActivity.java
+++ android/app/src/main/java/info/kyorohiro/ibeacontest/MainActivity.java
@@ -1,6 +1,9 @@
 package info.kyorohiro.ibeacontest;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
@@ -10,11 +13,64 @@ import io.flutter.view.FlutterView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 /**
  * Created by kyorohiro on 2017/01/13.
  */
 
 public class MainActivity extends Activity {
+    int requestIdBase = 0;
+    Map<Integer, FlutterView.MessageResponse> permissionResponse = new HashMap();
+
+    void requestLocationPermission(final FlutterView.MessageResponse messageResponse) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // need permission
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            } else {
+                JSONObject jsonMessage = new JSONObject();
+                try {
+                    jsonMessage.put("r", "ok");
+                } catch (JSONException e) {
+                }
+                messageResponse.send(jsonMessage.toString());
+            }
+        } else {
+            JSONObject jsonMessage = new JSONObject();
+            try {
+                jsonMessage.put("r", "ok");
+            } catch (JSONException e) {
+            }
+            messageResponse.send(jsonMessage.toString());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(permissionResponse.containsKey(requestCode)){
+            FlutterView.MessageResponse messageResponse = permissionResponse.remove(requestCode);
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                JSONObject jsonMessage = new JSONObject();
+                try {
+                    jsonMessage.put("r", "ok");
+                } catch(JSONException e){
+                }
+                messageResponse.send(jsonMessage.toString());
+            } else {
+                JSONObject jsonMessage = new JSONObject();
+                try {
+                    jsonMessage.put("r", "ok");
+                } catch(JSONException e){
+                }
+                messageResponse.send(jsonMessage.toString());
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
